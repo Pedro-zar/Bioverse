c5b907f7144de35f8a2a1e2bad3acbe90c8ce936
Pedro Henrique <pedrozar@outlook.com.br>
2025-03-15 00:00:49 -0300

    Removing unused helper functions

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,16 +1,3 @@
-/**
- * Format a date to the format dd/mm/yyyy.
- * @param date Date or Date compatible string .
- * @returns The date formatted as "dd/mm/yyyy".
- */
-export function formatDate(date: Date | string): string {
-    const d = new Date(date);
-    const day = d.getDate().toString().padStart(2, '0');
-    const month = (d.getMonth() + 1).toString().padStart(2, '0');
-    const year = d.getFullYear();
-    return `${day}/${month}/${year}`;
-  }
-  
   /**
    * Remove extra spaces of a string, with only one space left between words.
    * @param text The string to be clean.
